ccd4d11c3e91ca02859b832a510c4faadf1721bd
Cornel04 <104451188+Cornel04@users.noreply.github.com>
2025-01-17 11:57:07 +0200

    adding whole  project to @SpringBootApplication

--- src/main/java/proiectcolectiv/demo/DemoApplication.java
+++ src/main/java/proiectcolectiv/demo/DemoApplication.java
@@ -10,8 +10,7 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import proiectcolectiv.service.HospitalsService;
 
-@ComponentScan(basePackages = {"proiectcolectiv.mapper", "proiectcolectiv.controllers", "proiectcolectiv.service"})
-@SpringBootApplication(scanBasePackages = "proiectcolectiv.mapper")
+@SpringBootApplication(scanBasePackages = "proiectcolectiv")
 @EnableMongoRepositories
 public class DemoApplication implements CommandLineRunner {
 
